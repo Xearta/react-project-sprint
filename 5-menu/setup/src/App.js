43929e9d7f1dae27dc064b607163a5a3f427014e
Xearta <rsmcwow@gmail.com>
2020-11-23 13:57:16 -0500

    🔥 Menu Project Completed

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -3,8 +3,37 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// Set will only get the 'unique' values
+// This will add 'all' to the categories, then add every other UNIQUE category dynamically
+// all, breakfast, lunch, shakes
+const allCategories = ['all', ...new Set(items.map(item => item.category))];
+
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = category => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter(item => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories filterItems={filterItems} categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
