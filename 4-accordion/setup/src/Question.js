95c22b5eb4221052c0bd8c701429f65302d91359
Xearta <rsmcwow@gmail.com>
2020-11-23 13:20:00 -0500

    ⭐️ Accordian Project Completed

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ id, title, info }) => {
+  const [questionOpened, setQuestionOpened] = useState(false);
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button
+          className='btn'
+          onClick={() => setQuestionOpened(!questionOpened)}>
+          {questionOpened ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {questionOpened && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
