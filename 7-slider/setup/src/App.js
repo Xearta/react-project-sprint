d7de183808bd18e3526216a43d78ea4feaa83335
Xearta <rsmcwow@gmail.com>
2020-12-06 13:34:53 -0500

    Slider -> Made next & prev buttons work

--- 7-slider/setup/src/App.js
+++ 7-slider/setup/src/App.js
@@ -6,6 +6,18 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  // "Fix" for the index going out of bounds
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
   return (
     <section className='section'>
       <div className='title'>
@@ -40,10 +52,10 @@ function App() {
             </article>
           );
         })}
-        <button className='prev'>
+        <button className='prev' onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className='next'>
+        <button className='next' onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
