1940427f6eb9bd2a61c47d919d2fdd58ea1536a1
Xearta <rsmcwow@gmail.com>
2021-01-10 14:31:20 -0500

    Color Gen: Finished /w ability copy to clipboard

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -6,12 +6,26 @@ const SingleColor = ({ rgb, weight, index }) => {
   const bcg = rgb.join(',');
   const hex = rgbToHex(...rgb);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  const handleClick = () => {
+    setAlert(true);
+    navigator.clipboard.writeText(hex);
+  };
+
   return (
     <article
       className={`color ${index > 10 && 'color-light'}`}
-      style={{ backgroundColor: `rgb(${bcg})` }}>
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={handleClick}>
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hex}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
     </article>
   );
 };
